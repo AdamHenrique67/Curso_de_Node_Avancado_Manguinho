8e0c4005abc46f272d1b9dcbe01209936e7af02b
AdamHenrique67 <adamhenrique67@gmail.com>
2022-06-11 15:42:23 -0300

    ensure FacebookLoginController calls FacebookAuthentication with correct params

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,5 +1,10 @@
+import { FacebookAuthentication } from '@/domain/features'
+import { mock, MockProxy } from 'jest-mock-extended'
+
 class FacebookLoginController {
+  constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
   async handle (httpRequest: any): Promise<HttpResponse> {
+    await this.facebookAuthentication.perform({ token: httpRequest.token })
     return {
       statusCode: 400,
       data: new Error('The field token is required')
@@ -13,9 +18,18 @@ type HttpResponse = {
 }
 
 describe('FacebookLoginController', () => {
-  test('should return 400 if token is empty', async () => {
-    const sut = new FacebookLoginController()
+  let sut: FacebookLoginController
+  let facebookAuth: MockProxy<FacebookAuthentication>
 
+  beforeAll(() => {
+    facebookAuth = mock<FacebookAuthentication>()
+  })
+
+  beforeEach(() => {
+    sut = new FacebookLoginController(facebookAuth)
+  })
+
+  test('should return 400 if token is empty', async () => {
     const httpResponse = await sut.handle({ token: '' })
 
     expect(httpResponse).toEqual({
@@ -25,8 +39,6 @@ describe('FacebookLoginController', () => {
   })
 
   test('should return 400 if token is null', async () => {
-    const sut = new FacebookLoginController()
-
     const httpResponse = await sut.handle({ token: null })
 
     expect(httpResponse).toEqual({
@@ -36,13 +48,18 @@ describe('FacebookLoginController', () => {
   })
 
   test('should return 400 if token is undefined', async () => {
-    const sut = new FacebookLoginController()
-
     const httpResponse = await sut.handle({ token: undefined })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new Error('The field token is required')
     })
+  })
+
+  test('should call FacebookAuthentication with correct params', async () => {
+    await sut.handle({ token: 'any_token' })
+
+    expect(facebookAuth.perform).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(facebookAuth.perform).toHaveBeenCalledTimes(1)
   })
 })
