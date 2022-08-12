0f6bda760c523f3d39e232cc4f55340c825db8fb
AdamHenrique67 <adamhenrique67@gmail.com>
2022-08-12 14:56:24 -0300

    add facebook login integration tests

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -3,9 +3,9 @@ import { getRepository } from 'typeorm'
 import { PgUser } from '@/infra/postgres/entities'
 
 export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
-  private readonly pgUserRepo = getRepository(PgUser)
   async load ({ email }: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUser = await this.pgUserRepo.findOne({ email })
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ email })
     if (pgUser) {
       return {
         id: pgUser.id.toString(),
@@ -15,9 +15,10 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
   }
 
   async saveWithFacebook ({ id, name, email, facebookId }: SaveFacebookAccountRepository.Params): Promise<SaveFacebookAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
     let resultId: string
     if (!id) {
-      const pgUser = await this.pgUserRepo.save({
+      const pgUser = await pgUserRepo.save({
         email,
         name,
         facebookId
@@ -25,7 +26,7 @@ export class PgUserAccountRepository implements LoadUserAccountRepository, SaveF
       resultId = pgUser.id.toString()
     } else {
       resultId = id
-      await this.pgUserRepo.update({
+      await pgUserRepo.update({
         id: parseInt(id)
       }, {
         name: name,
--- tests/main/routes/login.spec.ts
+++ tests/main/routes/login.spec.ts
@@ -1,7 +1,52 @@
-describe('', () => {
-  describe('', () => {
-    test('', () => {
+import request from 'supertest'
+import { app } from '@/main/config/app'
+import { IBackup } from 'pg-mem'
+import { makeFakeDB } from '@/tests/infra/postgres/mocks'
+import { PgUser } from '@/infra/postgres/entities'
+import { getConnection } from 'typeorm'
+import { UnauthorizedError } from '@/application/errors'
 
+describe('Login routes', () => {
+  describe('POST /login/facebook', () => {
+    let backup: IBackup
+
+    const loadUserSpy = jest.fn()
+    jest.mock('@/infra/apis/facebook', () => ({
+      FacebookApi: jest.fn().mockReturnValue({
+        loadUser: loadUserSpy
+      })
+    }))
+
+    beforeAll(async () => {
+      const db = await makeFakeDB([PgUser])
+      backup = db.backup()
+    })
+
+    beforeEach(() => {
+      backup.restore()
+    })
+
+    afterAll(async () => {
+      await getConnection().close()
+    })
+
+    test('should return 200 with AccessToken', async () => {
+      loadUserSpy.mockResolvedValueOnce({ facebookId: 'any_id', name: 'any_name', email: 'any_email' })
+      const { statusCode, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'valid_token' })
+
+      expect(statusCode).toBe(200)
+      expect(body.accessToken).toBeDefined()
+    })
+
+    test('should return 401 with UnauthorizedError', async () => {
+      const { statusCode, body } = await request(app)
+        .post('/api/login/facebook')
+        .send({ token: 'invalid_token' })
+
+      expect(statusCode).toBe(401)
+      expect(body.error).toBe(new UnauthorizedError().message)
     })
   })
 })
