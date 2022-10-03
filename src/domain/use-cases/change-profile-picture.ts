965ca191c5bcdcaa2a59a763e8aa0090392ebe4c
AdamHenrique67 <adamhenrique67@gmail.com>
2022-10-03 07:45:45 -0300

    ensure ChangeProfilePicture calls SaveUserPicture with correct input

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -1,12 +1,14 @@
 import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
+import { SaveUserPicture } from '@/domain/contracts/repos'
 
 type Input = { id: string, file?: Buffer }
 export type ChangeProfilePicture = (input: Input) => Promise<void>
-type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture) => ChangeProfilePicture
 
-export const setupChangeProfilePicture: Setup = (fileStorage, crypto) => async ({ id, file }) => {
+export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfileRepo) => async ({ id, file }) => {
   if (file) {
     const uuid = crypto.generateUuid({ key: id })
-    await fileStorage.upload({ file, key: uuid })
+    const pictureUrl = await fileStorage.upload({ file, key: uuid })
+    await userProfileRepo.savePicture({ pictureUrl })
   }
 }
