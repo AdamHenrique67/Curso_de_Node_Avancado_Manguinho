19be8b67a93901a4d677940b11e1559a540f8ea2
AdamHenrique67 <adamhenrique67@gmail.com>
2022-10-10 18:29:41 -0300

    add aditional tests

--- tests/domain/entities/user-profile.spec.ts
+++ tests/domain/entities/user-profile.spec.ts
@@ -56,4 +56,14 @@ describe('UserProfile', () => {
       initials: undefined
     })
   })
+
+  test('Should create with empty initials when name and pictureUrl are not provided', () => {
+    sut.setPicture({ name: '' })
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      pictureUrl: undefined,
+      initials: undefined
+    })
+  })
 })
