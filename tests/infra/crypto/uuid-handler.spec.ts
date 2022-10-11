0c51793193449201bcc16730e68305f5026ce759
AdamHenrique67 <adamhenrique67@gmail.com>
2022-10-11 08:32:01 -0300

    move UUIDHandler to its own file

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -1,13 +1,7 @@
-import { UUIDGenerator } from '@/domain/contracts/gateways'
 import { v4 } from 'uuid'
+import { UUIDHandler } from '@/infra/crypto'
 
 jest.mock('uuid')
-
-class UUIDHandler {
-  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
-    return `${key}_${v4()}`
-  }
-}
 
 describe('UUIDHandler', () => {
   let sut: UUIDHandler
