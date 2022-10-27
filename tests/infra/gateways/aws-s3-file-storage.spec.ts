879ef4e7a60c3a59314a8b6c1da6647e2b2c320f
AdamHenrique67 <adamhenrique67@gmail.com>
2022-10-27 15:51:15 -0300

    move AwsS3FileStorage to its own file

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,30 +1,7 @@
-import { UploadFile } from '@/domain/contracts/gateways'
 import { config, S3 } from 'aws-sdk'
+import { AwsS3FileStorage } from '@/infra/gateways/aws-s3-file-storage'
 
 jest.mock('aws-sdk')
-
-class AwsS3FileStorage {
-  constructor (accessKey: string, secret: string, private readonly bucket: string) {
-    config.update({
-      credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secret
-      }
-    })
-  }
-
-  async upload ({ key, file }: UploadFile.Input): Promise<UploadFile.Output> {
-    const s3 = new S3()
-    await s3.putObject({
-      Bucket: this.bucket,
-      Key: key,
-      Body: file,
-      ACL: 'public-read'
-    }).promise()
-
-    return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(key)}`
-  }
-}
 
 describe('AwsS3FileStorage', () => {
   let sut: AwsS3FileStorage
