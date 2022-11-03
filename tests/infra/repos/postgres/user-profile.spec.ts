95606ee7d050bcc62863e15edad4c4cb14b09558
AdamHenrique67 <adamhenrique67@gmail.com>
2022-11-03 12:45:10 -0300

    ensure PgUserRepository loads user profile

--- tests/infra/repos/postgres/user-profile.spec.ts
+++ tests/infra/repos/postgres/user-profile.spec.ts
@@ -35,4 +35,14 @@ describe('PgUserAccountRepository', () => {
       expect(PgUser).toMatchObject({ id, pictureUrl: 'any_url', initials: null })
     })
   })
+
+  describe('loadPicture', () => {
+    test('should load user profile', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any_name' })
+
+      const userProfile = await sut.load({ id: id.toString() })
+
+      expect(userProfile?.name).toBe('any_name')
+    })
+  })
 })
