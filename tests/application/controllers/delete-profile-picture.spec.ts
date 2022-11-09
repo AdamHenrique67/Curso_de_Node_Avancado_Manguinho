956a8637e11d9ef3a2b64dbc706fabda26e19bce
AdamHenrique67 <adamhenrique67@gmail.com>
2022-11-09 14:58:55 -0300

    ensure DeletePictureController returns 204

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,19 +1,21 @@
 import { ChangeProfilePicture } from '@/domain/use-cases'
+import { HttpResponse, noContent } from '@/application/helpers'
 
 type HttpRequest = { userId: string }
 
 class DeletePictureController {
   constructor (private readonly changeProfilePicture: ChangeProfilePicture) {}
 
-  async handle ({ userId }: HttpRequest): Promise<void> {
+  async handle ({ userId }: HttpRequest): Promise<HttpResponse> {
     await this.changeProfilePicture({ id: userId })
+    return noContent()
   }
 }
 
 describe('DeletePictureController', () => {
   let changeProfilePicture: jest.Mock
   let sut: DeletePictureController
-  
+
   beforeAll(() => {
     changeProfilePicture = jest.fn() // jest.fn() pois agora os useCases sao funções
   })
@@ -22,10 +24,19 @@ describe('DeletePictureController', () => {
     sut = new DeletePictureController(changeProfilePicture)
   })
 
-  test('should call ChangeProfilePicture with correct inout', async () => {
+  test('should call ChangeProfilePicture with correct input', async () => {
     await sut.handle({ userId: 'any_user_id' })
 
     expect(changeProfilePicture).toHaveBeenCalledWith({ id: 'any_user_id' })
     expect(changeProfilePicture).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return 204', async () => {
+    const httpResponse = await sut.handle({ userId: 'any_user_id' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: null
+    })
   })
 })
