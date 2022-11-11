243c5c5d768bb098280db245b5d4e6631f984b5c
AdamHenrique67 <adamhenrique67@gmail.com>
2022-11-11 17:11:31 -0300

    add RequiredBuffer Validator

--- src/application/validation/required.ts
+++ src/application/validation/required.ts
@@ -3,7 +3,7 @@ import { Validator } from './validator'
 
 export class Required implements Validator {
   constructor (
-    readonly value: string,
+    readonly value: any,
     readonly fieldName?: string
   ) {}
 
@@ -22,8 +22,23 @@ export class RequiredString extends Required {
     super(value, fieldName)
   }
 
-  validate (): Error | undefined {
+  override validate (): Error | undefined {
     if (super.validate() !== undefined || this.value === '') {
+      return new RequiredFieldError(this.fieldName)
+    }
+  }
+}
+
+export class RequiredBuffer extends Required {
+  constructor (
+    override readonly value: Buffer,
+    override readonly fieldName?: string
+  ) {
+    super(value, fieldName)
+  }
+
+  override validate (): Error | undefined {
+    if (super.validate() !== undefined || this.value.length === 0) {
       return new RequiredFieldError(this.fieldName)
     }
   }
