5119d8eeb0202a115bae422056731c01ea124626
AdamHenrique67 <adamhenrique67@gmail.com>
2022-11-11 11:30:23 -0300

    ensure SavePictureController returns 400 if mimeType is invalid

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -1,24 +1,41 @@
 import { RequiredFieldError } from '@/application/errors'
 import { badRequest, HttpResponse } from '@/application/helpers'
 
-type HttpRequest = { file: any }
+type HttpRequest = { file: { buffer: Buffer, mimeType: string } }
 type Model = Error
 
 class SavePictureController {
   async perform ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
-    return badRequest(new RequiredFieldError('file'))
+    if (!file || file.buffer.length === 0) {
+      return badRequest(new RequiredFieldError('file'))
+    }
+    return badRequest(new InvalidMimeTypeError(['png', 'jpeg']))
+  }
+}
+
+class InvalidMimeTypeError extends Error {
+  constructor (allowed: string[]) {
+    super(`Unsopported type. Allowed types:${allowed.join(', ')}`)
+    this.name = 'InvalidMymeTypeError'
   }
 }
 
 describe('SavePictureController', () => {
+  let buffer: Buffer
+  let mimeType: string
   let sut: SavePictureController
+
+  beforeAll(() => {
+    buffer = Buffer.from('any_buffer')
+    mimeType = 'image/png'
+  })
 
   beforeEach(() => {
     sut = new SavePictureController()
   })
 
   test('should return 400 if file not provided', async () => {
-    const httpResponse = await sut.perform({ file: undefined })
+    const httpResponse = await sut.perform({ file: undefined as any })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
@@ -27,7 +44,7 @@ describe('SavePictureController', () => {
   })
 
   test('should return 400 if file not provided', async () => {
-    const httpResponse = await sut.perform({ file: null })
+    const httpResponse = await sut.perform({ file: null as any })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
@@ -36,11 +53,20 @@ describe('SavePictureController', () => {
   })
 
   test('should return 400 if file is empty', async () => {
-    const httpResponse = await sut.perform({ file: { buffer: Buffer.from('') } })
+    const httpResponse = await sut.perform({ file: { buffer: Buffer.from(''), mimeType } })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new RequiredFieldError('file')
+    })
+  })
+
+  test('should return 400 if file type is invalid', async () => {
+    const httpResponse = await sut.perform({ file: { buffer, mimeType: 'invalid_type' } })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new InvalidMimeTypeError(['png', 'jpeg'])
     })
   })
 })
