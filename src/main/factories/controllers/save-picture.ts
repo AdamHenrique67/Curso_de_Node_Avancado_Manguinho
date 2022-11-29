d50797001a2a1d497bc24449e958877fcc109062
AdamHenrique67 <adamhenrique67@gmail.com>
2022-11-29 19:39:03 -0300

    add PgTransactionController factory

--- src/main/factories/controllers/save-picture.ts
+++ src/main/factories/controllers/save-picture.ts
@@ -1,6 +1,8 @@
-import { SavePictureController } from '@/application/controllers'
+import { Controller, SavePictureController } from '@/application/controllers'
 import { makeCangeProfilePicture } from '@/main/factories/use-cases'
+import { makePgTransactionController } from '../decorators'
 
-export const makeSavePictureController = (): SavePictureController => {
-  return new SavePictureController(makeCangeProfilePicture())
+export const makeSavePictureController = (): Controller => {
+  const controller = new SavePictureController(makeCangeProfilePicture())
+  return makePgTransactionController(controller)
 }
